545f1ab5ef74c97dadb9595bf884e7f5813007f3
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2026-09-15 09:16:25 +0200

    commit

--- ElatkozottBurok/Fejleszto.cs
+++ ElatkozottBurok/Fejleszto.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
 
 namespace ElatkozottBurok
 {
     public class Fejleszto
     {
-    
+        private string nev;
+        private Munkakor munkakor;
+        private int penz;
+        private int koffeinSzint;
+        private int stresszSzint;
+        private bool kiegve;
+        private string kedvencSnack;
     }
 }
