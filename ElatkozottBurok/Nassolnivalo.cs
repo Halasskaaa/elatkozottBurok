16887158c870daedb51fa23f097e468b6911ab8a
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2026-09-15 09:18:59 +0200

    Reapply "commit"
    This reverts commit 6c3c76b931b79921a6518094da8ae5b3ea597166.

--- ElatkozottBurok/Nassolnivalo.cs
+++ ElatkozottBurok/Nassolnivalo.cs
@@ -12,7 +12,7 @@ namespace ElatkozottBurok
         private int stresszOldas;
         private int ar;
 
-        public Nassolnivalo(string nev, int stresszOldas, int ar, int koffeinLoket)
+        public Nassolnivalo(string nev, int koffeinLoket, int stresszOldas, int ar)
         {
             this.nev = nev;
             this.koffeinLoket = koffeinLoket;
@@ -36,9 +36,13 @@ namespace ElatkozottBurok
         public int KoffeinLoket { 
             get => koffeinLoket; 
             set {
-                if (koffeinLoket < 0 || koffeinLoket > 50)
+                if (koffeinLoket < 0)
                 {
-                    Console.WriteLine("Koffein szintnek 0 és 50 között kell lennie.");
+                    koffeinLoket = 0;
+                }
+                else if (koffeinLoket > 50)
+                {
+                    koffeinLoket = 50;
                 }
                 else
                 {
@@ -49,9 +53,22 @@ namespace ElatkozottBurok
         public int StresszOldas { 
             get => stresszOldas; 
             set {
+                if (stresszOldas < 0 || stresszOldas > 30)
+                {
+                    Console.WriteLine("Stressz csökkentés 0 és 30 között lehet.");
+                }
                 stresszOldas = value; 
             }
         }
-        public int Ar { get => ar; set => ar = value; }
+        public int Ar { 
+            get => ar; 
+            set {
+                if (ar  < 100)
+                {
+                    Console.WriteLine("Az árnak minimum 100-nka kell lennie.");
+                }
+                ar = value; 
+            } 
+        }
     }
 }
